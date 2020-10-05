1c93452a3bea111f6cd18dc87794434a5eed95b1
Bene Caspari <skuriles@gmx.de>
2020-10-05 20:07:46 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StartComponent } from "./app.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule } from "@angular/forms";
+
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [StartComponent, LoginComponent],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    FormsModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [StartComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+})
+export class LoginComponent implements OnInit {
+  public name = "";
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public checkName() {
+    return false;
+  }
+}
